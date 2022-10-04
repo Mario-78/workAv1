e0f3336d24668d0c2b001b418d1b2f61a172ab77
Mario-78 <mariojsws1@gmail.com>
2022-10-04 18:37:34 -0300

    Mocando dados

--- src/workAv1/Main.java
+++ src/workAv1/Main.java
@@ -7,6 +7,7 @@ import javax.swing.JOptionPane;
 import workAv1_recursosHumanos.Aluno;
 import workAv1_recursosHumanos.Coordenador;
 import workAv1_recursosHumanos.Diretor;
+import workAv1_recursosHumanos.Professor;
 
 public class Main {
 
@@ -14,14 +15,30 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int op = 0, idPessoa = 0, idEmpresa = 0, matri = 0;
+		int op = 0, idPessoa = 0, idEmpresa = 0, matri = 100;
 		
 		Aluno aluno = new Aluno();
 		Coordenador coordenador = new Coordenador();
 		Diretor diretor = new Diretor();
 		
 		
-		aluno = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno0 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno1 = new Aluno(++idPessoa, "Antonio Pereira Silva", "12457896320", ++matri);
+		Aluno aluno2 = new Aluno(++idPessoa, "Claudio Jose Martins", "15975326501", ++matri);
+		Aluno aluno3 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno4 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno5 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno6 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno7 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno8 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		Aluno aluno9 = new Aluno(++idPessoa, "João Marcos", "15975326501", ++matri);
+		
+		Professor professor0 = new Professor(++idPessoa, "Paulo Sousa", "45695135712", "professor");
+		Professor professor0 = new Professor(++idPessoa, "Paulo Sousa", "45695135712", "professor");
+		Professor professor0 = new Professor(++idPessoa, "Paulo Sousa", "45695135712", "professor");
+		
+		Coordenador coordenador0 = new Coordenador(++idPessoa, "Mariana Silva", "12325845695", "coordenador");
+		Coordenador coordenador0 = new Coordenador(++idPessoa, "Mariana Silva", "12325845695", "coordenador");
 		
 		op = Integer.parseInt(JOptionPane.showInputDialog("Informe a opção desejada: \n"
 				+ "01 Para cadastrar uma pessoa \n"
